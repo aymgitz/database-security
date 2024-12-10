8c96fdc6e3445c132624e67493f413d69af8b005
anyonez <gitzforproject@gmail.com>
2024-12-11 07:47:37 +0800

    Add database query statement

--- database.php
+++ database.php
@@ -14,7 +14,8 @@
  create database rd_folder2;
   use rd_folder2;
   create table accounts(id int primary key auto_increment not null, email varchar(255) unique not null, password varchar(255) not null);
-  
+
+  create table personal_info(id int primary key auto_increment, accounts_id int, firstname varchar(100), middlename varchar(100),lastname varchar(100), birthdate date, address varchar(100), gender enum('male', 'female', 'other'), foreign key(accounts_id) references accounts(id));
  */
 
 
